d8318a37d62128b7acb4c4a139c669fc77849608
Henrik Liman <Alpner93@gmail.com>
2020-09-14 13:53:29 +0200

    in add42 coment

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,10 @@
 FIZZ = 3
 BUZZ = 5
 FIZZBUZZ = FIZZ * BUZZ
-
+#in add42
 def main():
     for i in range(1, 101):
-        
+        1
         if i % FIZZBUZZ == 0:
             print("Fizzbuzz")
         elif i % FIZZ == 0:
