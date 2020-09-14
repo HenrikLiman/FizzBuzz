30dd9f3acdb6c5c0c3f1b806cabe89b594678a99
Henrik Liman <Alpner93@gmail.com>
2020-09-14 14:04:12 +0200

    if 42 print meaning of life

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,13 +4,14 @@ FIZZBUZZ = FIZZ * BUZZ
 #in add42
 def main():
     for i in range(1, 101):
-        1
         if i % FIZZBUZZ == 0:
             print("Fizzbuzz")
         elif i % FIZZ == 0:
             print("Fizz")
         elif i % BUZZ == 0:
             print("Buzz")
+        elif i == 42:
+            print("Answer to the Ultimate Question of Life, the Universe, and Everything")
         else:
             print(i)
         
